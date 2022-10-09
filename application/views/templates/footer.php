5f9b0d112d8d0d16fb2a83492bb5f13d0df3e697
Sulthan Nurya <53323434+shym2501@users.noreply.github.com>
2022-10-09 21:54:43 +0000

    Create

---
+++ application/views/templates/footer.php
@@ -0,0 +1,17 @@
+<footer id="footer" class="footer">
+         <div class="copyright"> &copy; Copyright <strong><span>Compnay Name</span></strong>. All Rights Reserved</div>
+         <div class="credits"> with love <a href="https://freeetemplates.com/">FreeeTemplates</a></div>
+      </footer>
+      <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>  
+        <script src="assets/js/apexcharts.min.js"></script>
+        <script src="assets/js/bootstrap.bundle.min.js"></script>
+        <script src="assets/js/chart.min.js"></script>
+        <script src="assets/js/echarts.min.js"></script>
+        <script src="assets/js/quill.min.js"></script>
+        <script src="assets/js/simple-datatables.js"></script>
+        <script src="assets/js/tinymce.min.js"></script>
+        <script src="assets/js/validate.js"></script>
+        <script src="assets/js/main.js"></script> 
+             
+   </body>
+</html>
