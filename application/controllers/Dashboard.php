185c75539587ff6d7547cc74fe8c400298682f85
Sulthan Nurya <>
2022-10-10 02:19:33 +0000

    Create

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -6,6 +6,8 @@ class Dashboard extends CI_Controller {
 	public function index()
 	{
     $this->load->view('templates/header');
+    $this->load->view('templates/sidebar');
+    $this->load->view('dashboard/index');
 		$this->load->view('templates/footer');
 	}
 }
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -7,18 +7,18 @@
       <meta name="robots" content="noindex, nofollow">
       <meta content="" name="description">
       <meta content="" name="keywords">
-      <link href="assets/img/favicon.png" rel="icon">
-      <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+      <link href="<?= base_url('assets/');?>img/favicon.png" rel="icon">
+      <link href="<?= base_url('assets/');?>img/apple-touch-icon.png" rel="apple-touch-icon">
       <link href="https://fonts.gstatic.com" rel="preconnect">
       <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-      <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+      <link href="<?= base_url('assets/'); ?>css/bootstrap.min.css" rel="stylesheet">
       <link href="assets/css/bootstrap-icons.css" rel="stylesheet">
       <link href="assets/css/boxicons.min.css" rel="stylesheet">
-      <link href="assets/css/quill.snow.css" rel="stylesheet">
-      <link href="assets/css/quill.bubble.css" rel="stylesheet">
+      <link href="<?= base_url('assets/'); ?>css/quill.snow.css" rel="stylesheet">
+      <link href="<?= base_url('assets/'); ?>css/quill.bubble.css" rel="stylesheet">
       <link href="assets/css/remixicon.css" rel="stylesheet">
-      <link href="assets/css/simple-datatables.css" rel="stylesheet">
-      <link href="assets/css/style.css" rel="stylesheet">
+      <link href="<?= base_url('assets/'); ?>css/simple-datatables.css" rel="stylesheet">
+      <link href="<?= base_url('assets/'); ?>css/style.css" rel="stylesheet">
    </head>
    <body>
       <header id="header" class="header fixed-top d-flex align-items-center">
